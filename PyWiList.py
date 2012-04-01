c95fe4d336f36c9322e99b16311291abfb66b387
Christopher Martin <ch.martin@gmail.com>
2012-04-01 14:18:53 -0400

    read interface name from sys.argv

--- PyWiList.py
+++ PyWiList.py
@@ -6,7 +6,7 @@ log = logging.getLogger("PyWiList")
 log.setLevel(logging.DEBUG)
 
 if __name__  ==  '__main__':
-    iwl = IWList("eth1")
+    iwl = IWList(sys.argv[1])
     print iwl.getData()
 
     
